30f1bc0554e2942296fa53c57c4b1e96cd9e3275
Piotr Andrzejewski <pandrz@acxiom.com>
2018-10-29 13:01:29 +0100

    connect to redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import './index.css';
-import App from './App';
+import Counter from './Counter';
 import * as serviceWorker from './serviceWorker';
+
+const initialState = {count: 0}
+
+const reducer = ( state = initialState, action) => {
+  console.log(action);
+  switch(action.type) {
+    case 'INCREMENT':
+      return {
+        count: state.count + 1
+      }
+    case 'DECREMENT':
+      return {
+        count: state.count - 1
+      }
+    default: return state
+  }
+
+}
+const store = createStore(reducer);
+
+const App = () => (
+ <Provider store={store}>
+  <Counter/>
+ </Provider>
+)
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
